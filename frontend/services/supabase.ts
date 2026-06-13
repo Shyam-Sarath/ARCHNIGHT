c622bca7938f168ea294b24c9f472e290e53a713
Shyam Sarath <shyam.sarath@example.com>
2026-06-13 22:37:22 +0530

    fix: fallback to actual supabase anon key during static page prerendering

--- frontend/services/supabase.ts
+++ frontend/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://zufwhvweywjubyvbeeza.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp1ZndodndleXdqdWJ5dmJlZXphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNTA4ODQsImV4cCI6MjA5NjkyNjg4NH0.PIZ1m-kTkb1pVdDHARo4rHCN_cF4HPrpDupL0YxMnEY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
